94bfcb17641174b2c21ccf70afc20b9605a601c9
Terje Kvernes <terje@kvernes.no>
2021-09-17 12:22:23 +0200

    Add IPv6 support for stratum1

--- static/terraform/security.tf
+++ static/terraform/security.tf
@@ -66,6 +66,7 @@ resource "aws_security_group" "stratum1" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -73,6 +74,7 @@ resource "aws_security_group" "stratum1" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -80,5 +82,6 @@ resource "aws_security_group" "stratum1" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
